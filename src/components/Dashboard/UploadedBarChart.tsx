20a8612f32acc82d261c7d459bd6654e32b8730a
Charles <jinyongchor@vimlogy.com>
2025-11-18 09:47:01 +0800

    refactor: change empty bar chart ui

--- src/components/Dashboard/UploadedBarChart.tsx
+++ src/components/Dashboard/UploadedBarChart.tsx
@@ -4,30 +4,29 @@ import {
 } from "antd"
 import StackedBar from "components/ui/Charts/StackedBar"
 import useApi from "hooks/useApi";
-import { MehOutlined } from "@ant-design/icons"
+import { Empty } from 'antd';
 
 export default function UploadedBarChart() {
-    const { data, loading } = useApi<{ 
+    const { data, loading } = useApi<{
         monthYear: string;
         type: 'bookmark' | 'active' | 'inactive';
-        value: number 
+        value: number
     }[]>('/api/panoramas/analytics/bar-chart');
 
     return(
         <Card title="Uploaded Panoramas" style={{ minHeight: 240, display: 'block' }}>
-            { data && data.length > 0 && !loading ? 
+            { data && data.length > 0 && !loading ?
                 <StackedBar data={data || []} />
-            : 
-                <div style={{ 
-                    height: 300,  
-                    display: 'flex', 
-                    flexDirection: 'column', 
-                    alignItems: 'center', 
-                    justifyContent: 'center', 
-                    color: '#999'   
+            :
+                <div style={{
+                    height: 300,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: '#999'
                 }}>
-                    <MehOutlined style={{ fontSize: 26 }} />
-                    Not enough data to display chart.
+                    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
                 </div>
             }
         </Card>
